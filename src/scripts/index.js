afab20c9205cd73a29ecccb5a027fd8d5b54516a
Yyyurii <yurii.kryvko@gmail.com>
2020-04-14 21:41:32 +0300

    Відредагував шрифти, відступи і доб. курсорпоінтер

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,18 @@
 import $ from 'jquery';
 
-const onDocumentReady = () => {};
+$(document).ready(function onDocumentReady() {
 
-$(document).ready(onDocumentReady);
+// $('.insta-images').slick();
+
+// $('.vinsent-left-column__vinsent-review').slick({
+//       infinite: true,
+//       slidesToShow: 3,
+//       slidesToScroll: 3
+//     });
+
+
+
+    // $(".left-column__bottle").sticky({topSpacing:0});
+
+});
+
