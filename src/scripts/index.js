1c2b36e4986690167394d8dfff1d318609880a7b
Yyyurii <yurii.kryvko@gmail.com>
2020-04-19 16:03:39 +0300

    додав лічильник для -

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,19 @@ import $ from 'jquery';
 
 $(document).ready(function onDocumentReady() {
     var $one = $('.one')
+    var val = $('.one').html();
     $('.plus').on('click', function () {
-        var val = $('.one').html();
         val++;
         $one.empty();
         $one.prepend(val);
+    });
+
+    $('.minus').on('click', function () {
+        if (val > 1) {
+            val--;
+            $one.empty();
+            $one.prepend(val);
+        }      
     });
     
 });
