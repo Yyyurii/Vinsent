8016a0a30a4032f5adbc62d7d4ff5a74d1ea0800
Yyyurii <yurii.kryvko@gmail.com>
2020-04-19 16:00:50 +0300

    додав лічильник для +

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,12 @@
 import $ from 'jquery';
 
 $(document).ready(function onDocumentReady() {
-
-// $('.insta-images').slick();
-
-// $('.vinsent-left-column__vinsent-review').slick({
-//       infinite: true,
-//       slidesToShow: 3,
-//       slidesToScroll: 3
-//     });
-// console.log($('.vinsent-left-column__vinsent-review'))  
-
-
-    // $(".left-column__bottle").sticky({topSpacing:0});
-mn
+    var $one = $('.one')
+    $('.plus').on('click', function () {
+        var val = $('.one').html();
+        val++;
+        $one.empty();
+        $one.prepend(val);
+    });
+    
 });
-
